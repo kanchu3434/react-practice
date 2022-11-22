412b9ba51df6ff58a7c215004546185afe8e5670
kanchan <kanchankotkar001@gmail.com>
2022-11-22 14:46:36 +0530

    initial commit

---
+++ src/mango.js
@@ -0,0 +1,12 @@
+ import "./Mango.css"
+function Mango(){
+    return(
+      <div className="mango-container">
+        <h1>
+        Mango 💛
+      </h1>
+      </div>
+        
+    )
+}
+export default Mango;
